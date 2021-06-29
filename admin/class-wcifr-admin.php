99b69eadb52324eb409862be26c67b4053d61eb6
ilGhera <andrea@ilghera.com>
2021-06-29 08:19:31 +0200

    Rimossi iframe in admin

--- admin/class-wcifr-admin.php
+++ admin/class-wcifr-admin.php
@@ -136,9 +136,7 @@ class WCIFR_Admin {
 
 			echo '</div>';
 
-			echo '<div class="wrap-right">';
-				echo '<iframe width="300" height="900" scrolling="no" src="https://www.ilghera.com/images/wcifr-premium-iframe.html"></iframe>';
-			echo '</div>';
+			echo '<div class="wrap-right"></div>';
 
 			echo '<div class="clear"></div>';
 
