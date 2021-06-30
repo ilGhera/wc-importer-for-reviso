16eecd97855db9290b4e85bc639a6031e6044679
ilGhera <andrea@ilghera.com>
2021-06-30 17:36:18 +0200

    Revert "Rimossi iframe in admin"
    This reverts commit 99b69eadb52324eb409862be26c67b4053d61eb6.

--- admin/class-wcifr-admin.php
+++ admin/class-wcifr-admin.php
@@ -136,7 +136,9 @@ class WCIFR_Admin {
 
 			echo '</div>';
 
-			echo '<div class="wrap-right"></div>';
+			echo '<div class="wrap-right">';
+				echo '<iframe width="300" height="900" scrolling="no" src="https://www.ilghera.com/images/wcifr-premium-iframe.html"></iframe>';
+			echo '</div>';
 
 			echo '<div class="clear"></div>';
 
