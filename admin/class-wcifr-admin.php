2d48e70fe0ded69b13f394ebc9a51814dd7c0e5f
ilGhera <andrea@ilghera.com>
2021-06-30 17:55:38 +0200

    Cambio iframe in admin sidebar

--- admin/class-wcifr-admin.php
+++ admin/class-wcifr-admin.php
@@ -137,7 +137,7 @@ class WCIFR_Admin {
 			echo '</div>';
 
 			echo '<div class="wrap-right">';
-				echo '<iframe width="300" height="900" scrolling="no" src="https://www.ilghera.com/images/wcifr-premium-iframe.html"></iframe>';
+				echo '<iframe width="300" height="900" scrolling="no" src="https://www.ilghera.com/images/wcifr-iframe.html"></iframe>';
 			echo '</div>';
 
 			echo '<div class="clear"></div>';
