8a654841d2a2f34c4a501c918505f6b56c12b123
ilGhera <andrea@ilghera.com>
2025-06-24 15:24:15 +0200

    Coding standard classe admin No accesso diretto classe admin

--- admin/class-wcifr-admin.php
+++ admin/class-wcifr-admin.php
@@ -4,6 +4,15 @@
  *
  * @author ilGhera
  * @package wc-importer-for-reviso/admin
+ *
+ * @since 0.9.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * WCIFR_Admin
+ *
  * @since 0.9.0
  */
 class WCIFR_Admin {
@@ -15,9 +24,7 @@ class WCIFR_Admin {
 
 		add_action( 'admin_menu', array( $this, 'wcifr_add_menu' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'wcifr_register_scripts' ) );
-
 	}
-
 
 	/**
 	 * Scripts and style sheets
@@ -27,24 +34,22 @@ class WCIFR_Admin {
 	public function wcifr_register_scripts() {
 
 		$screen = get_current_screen();
+
 		if ( 'woocommerce_page_wc-importer-for-reviso' === $screen->id ) {
 
 			/*js*/
 			wp_enqueue_script( 'wcifr-js', WCIFR_URI . 'js/wcifr.js', array( 'jquery' ), '1.0', true );
 
 			/*css*/
-			wp_enqueue_style( 'bootstrap-iso', plugin_dir_url( __DIR__ ) . 'css/bootstrap-iso.css' );
+			wp_enqueue_style( 'bootstrap-iso', plugin_dir_url( __DIR__ ) . 'css/bootstrap-iso.css', array(), WCIFR_VERSION );
 
 		} elseif ( 'edit-shop_order' === $screen->id ) {
 
 			wp_enqueue_script( 'wcifr-js', WCIFR_URI . 'js/wcifr-shop-orders.js', array( 'jquery' ), '1.0', true );
-
 		}
 
-		wp_enqueue_style( 'wcifr-style', WCIFR_URI . 'css/wc-importer-for-reviso.css' );
-
+		wp_enqueue_style( 'wcifr-style', WCIFR_URI . 'css/wc-importer-for-reviso.css', array(), WCIFR_VERSION );
 	}
-
 
 	/**
 	 * Menu page
@@ -56,9 +61,7 @@ class WCIFR_Admin {
 		$wcifr_page = add_submenu_page( 'woocommerce', 'WCIFR Options', __( 'WC Importer for Reviso', 'wc-importer-for-reviso' ), 'manage_woocommerce', 'wc-importer-for-reviso', array( $this, 'wcifr_options' ) );
 
 		return $wcifr_page;
-
 	}
-
 
 	/**
 	 * Options page
@@ -71,7 +74,6 @@ class WCIFR_Admin {
 		if ( ! current_user_can( 'manage_woocommerce' ) ) {
 
 			wp_die( esc_html( __( 'It seems like you don\'t have permission to see this page', 'wc-importer-for-reviso' ) ) );
-
 		}
 
 		/*Page template start*/
@@ -79,14 +81,16 @@ class WCIFR_Admin {
 			echo '<div class="wrap-left">';
 
 				/*Check if WooCommerce is installed ancd activated*/
-				if ( ! class_exists( 'WooCommerce' ) ) {
-					echo '<div id="message" class="error">';
-						echo '<p>';
-							echo '<strong>' . esc_html( __( 'ATTENTION! It seems like Woocommerce is not installed', 'wc-importer-for-reviso' ) ) . '</strong>';
-						echo '</p>';
-					echo '</div>';
-					exit;
-				}
+		if ( ! class_exists( 'WooCommerce' ) ) {
+
+			echo '<div id="message" class="error">';
+				echo '<p>';
+					echo '<strong>' . esc_html( __( 'ATTENTION! It seems like Woocommerce is not installed', 'wc-importer-for-reviso' ) ) . '</strong>';
+				echo '</p>';
+			echo '</div>';
+
+			exit;
+		}
 
 				echo '<div id="wcifr-generale">';
 
@@ -96,13 +100,12 @@ class WCIFR_Admin {
 					/*Plugin premium key*/
 					$key = sanitize_text_field( get_option( 'wcifr-premium-key' ) );
 
-					if ( isset( $_POST['wcifr-premium-key'], $_POST['wcifr-premium-key-nonce'] ) && wp_verify_nonce( wp_unslash( $_POST['wcifr-premium-key-nonce'] ), 'wcifr-premium-key' ) ) {
+		if ( isset( $_POST['wcifr-premium-key'], $_POST['wcifr-premium-key-nonce'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['wcifr-premium-key-nonce'] ) ), 'wcifr-premium-key' ) ) {
 
-						$key = sanitize_text_field( wp_unslash( $_POST['wcifr-premium-key'] ) );
+			$key = sanitize_text_field( wp_unslash( $_POST['wcifr-premium-key'] ) );
 
-						update_option( 'wcifr-premium-key', $key );
-
-					}
+			update_option( 'wcifr-premium-key', $key );
+		}
 
 					/*Premium Key Form*/
 					echo '<form id="wcifr-premium-key" method="post" action="">';
@@ -125,28 +128,28 @@ class WCIFR_Admin {
 					/*Settings*/
 					echo '<div id="wcifr-settings" class="wcifr-admin" style="display: block;">';
 
-						include( WCIFR_ADMIN . 'wcifr-settings-template.php' );
+						include WCIFR_ADMIN . 'wcifr-settings-template.php';
 
 					echo '</div>';
 
 					/*Suppliers*/
 					echo '<div id="wcifr-suppliers" class="wcifr-admin">';
 
-						include( WCIFR_ADMIN . 'wcifr-suppliers-template.php' );
+						include WCIFR_ADMIN . 'wcifr-suppliers-template.php';
 
 					echo '</div>';
 
 					/*Products*/
 					echo '<div id="wcifr-products" class="wcifr-admin">';
 
-						include( WCIFR_ADMIN . 'wcifr-products-template.php' );
+						include WCIFR_ADMIN . 'wcifr-products-template.php';
 
 					echo '</div>';
 
 					/*Customers*/
 					echo '<div id="wcifr-customers" class="wcifr-admin">';
 
-						include( WCIFR_ADMIN . 'wcifr-customers-template.php' );
+						include WCIFR_ADMIN . 'wcifr-customers-template.php';
 
 					echo '</div>';
 
@@ -167,8 +170,8 @@ class WCIFR_Admin {
 			echo '<div class="clear"></div>';
 
 		echo '</div>';
-
 	}
-
 }
+
 new WCIFR_Admin();
+
